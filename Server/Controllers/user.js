d0a46ddf0117c8f6b12728b6f8577ba890b18dcc
prathambhalke <prathameshbhalke125@gmail.com>
2024-04-10 11:32:32 +0530

    added the services model in folder structure for handling the uniqueID

--- Server/Controllers/user.js
+++ Server/Controllers/user.js
@@ -1,5 +1,6 @@
-const { use } = require("passport");
 const User = require("../Models/usersData");
+const { v4: uuidv4 } = require("uuid");
+const { getUser, setUser } = require("../Services/auth");
 
 async function handleUserSignUp(req, res) {
   const { name, email, password } = req.body;
@@ -15,10 +16,13 @@ async function handleUserSignUp(req, res) {
   }
 }
 
-async function handleUserLogin(req, res) {
+async function handleUserLogin(req, res) {  
   const { name, password } = req.body;
   const logUser = await User.findOne({ name, password });
   if (!logUser) return res.status(404).json("Invalid UserName or Password⚠️");
+  const sessionId = uuidv4();
+  setUser(sessionId, logUser);
+  res.cookie("uid", sessionId);
   return res.status(204).json(`${name} Login SuccessFully🚀!`);
 }
 
