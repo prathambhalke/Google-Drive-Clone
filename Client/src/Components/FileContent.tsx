b5d598a2bdc10e873bc6ad214ce1a1f4b2a8fa22
prathambhalke <prathameshbhalke125@gmail.com>
2024-04-28 00:48:39 +0530

    UI changes

--- Client/src/Components/FileContent.tsx
+++ Client/src/Components/FileContent.tsx
@@ -1,9 +1,21 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 const FileContent = ({activeTab} : any) => {
+const [data, setData] = useState([]);
+  useEffect(()=>{
+    axios.get("http://localhost:5002/upload/getFileData").then(res => setData(res.data.data)).catch(err => err)
+  },[])
   return (
     <div className="flex flex-col w-[75%] bg-white rounded-xl">
     {/* Content based on active tab */}
     {activeTab === 'myDrive' && (
-      <div className="p-4">My Drive Content</div>
+      <div className="p-4">
+        <span>My Drive Content</span>
+      {/* {
+        data?.data.map(item => <img src={item}/>)
+      } */}
+      </div>
     )}
     {activeTab === 'starred' && (
       <div className="p-4">Starred Content</div>
--- Client/src/Components/Home/SideBar.tsx
+++ Client/src/Components/Home/SideBar.tsx
@@ -7,25 +7,46 @@ import { MdOutlineCreateNewFolder } from "react-icons/md";
 import { MdUploadFile } from "react-icons/md";
 import FileContent from "../FileContent";
 import CreateFolder from "../Popups/CreateFolder";
+import axios from "axios";
+import { json } from "react-router-dom";
 const SideBar = () => {
   const [activeTab, setActiveTab] = useState("myDrive");
   const [newSelectVisible, setNewSelectVisible] = useState<boolean>(false);
-  const [createFolderVisible, setCreateFolderVisible] = useState<boolean>(false);
+  const [createFolderVisible, setCreateFolderVisible] =
+    useState<boolean>(false);
+  const [fileData, setFileData] = useState("");
   const Ref = useRef(null);
-  const changeTab = (tabName : any) => {
+  const changeTab = (tabName: any) => {
     setActiveTab(tabName);
   };
 
   const handleNewSelectClick = () => {
     setNewSelectVisible(!newSelectVisible);
   };
-const onFileUpload = () => {
-  Ref.current.click()
-}
+  const onFileUpload = () => {
+    Ref.current.click();
+  };
 
-const onFolderUpload = () => {
-  setCreateFolderVisible(!createFolderVisible)
-}
+  const onFolderUpload = () => {
+    setCreateFolderVisible(!createFolderVisible);
+  };
+  const onfileSelect = (e: any) => {
+    // Set the file data to the selected file
+    setFileData(e.target.files[0]);
+  };
+
+  const onUploadClick = (e: any) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    const formData = new FormData()
+    formData.append("fileData", fileData);
+
+    axios
+      .post("http://localhost:5002/upload/fileUpload", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((res) => console.log("res", res))
+      .catch((err) => console.log("err", err));
+  };
   // Array of buttons
   const buttons = [
     { icon: <FaGoogleDrive />, label: "My Drive", tab: "myDrive" },
@@ -40,17 +61,35 @@ const onFolderUpload = () => {
         <div>
           {newSelectVisible && (
             <div className="absolute items-center top-1 left-10 min-w-72 bg-white shadow-lg rounded-lg py-4">
-              <button className="flex items-center w-full py-2 mb-2 border-b-2 hover:bg-gray-200" onClick={onFolderUpload}>
+              <button
+                className="flex items-center w-full py-2 mb-2 border-b-2 hover:bg-gray-200"
+                onClick={onFolderUpload}
+              >
                 <MdOutlineCreateNewFolder size={24} className="mx-4" />
                 New Folder
               </button>
-              <button className="flex items-center w-full py-2 hover:bg-gray-200" onClick={onFileUpload}>
+              <button
+                className="flex items-center w-full py-2 hover:bg-gray-200"
+                onClick={onFileUpload}
+              >
                 <MdUploadFile size={24} className="mx-4" />
                 File Upload
               </button>
-              <form action="/upload" method="POST" encType="multipart/form-data">
-              <input type="file" name="fileupload" ref={Ref} id="fileupload" />
-              <button type="submit">upload</button>
+              <form onSubmit={onUploadClick}>
+                <input
+                  type="file"
+                  name="fileupload"
+                  ref={Ref}
+                  id="fileupload"
+                  hidden
+                  onChange={onfileSelect}
+                />
+                <button
+                  type="submit"
+                  className="flex items-center w-full py-2 hover:bg-gray-200"
+                >
+                  upload
+                </button>
               </form>
             </div>
           )}
@@ -79,10 +118,13 @@ const onFolderUpload = () => {
         </div>
       </div>
       <FileContent activeTab={activeTab} />
-      {
-        createFolderVisible && <CreateFolder FolderVisibility={createFolderVisible} setCreateFolderVisible={setCreateFolderVisible} inputRef={Ref}/>
-      }
-       
+      {createFolderVisible && (
+        <CreateFolder
+          FolderVisibility={createFolderVisible}
+          setCreateFolderVisible={setCreateFolderVisible}
+          inputRef={Ref}
+        />
+      )}
     </div>
   );
 };
