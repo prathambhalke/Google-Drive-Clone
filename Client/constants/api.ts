693187a8c670ab302e4c06707ef1dc4bf2beaebc
prathambhalke <prathameshbhalke125@gmail.com>
2024-05-28 22:53:18 +0530

    changed the get request URL to local

--- Client/constants/api.ts
+++ Client/constants/api.ts
@@ -1,4 +1,4 @@
-export const REDIRECTFILE = "https://google-drive-clone-v1-git-main-prathameshbhalkes-projects.vercel.app/uploads"
+export const REDIRECTFILE = "http://localhost:5002/uploads"
 export const GETFILEDATA = "https://google-drive-clone-v1-git-main-prathameshbhalkes-projects.vercel.app/upload/getFileData";
 
 export const UPLOADFILEDATA = "https://google-drive-clone-v1-git-main-prathameshbhalkes-projects.vercel.app/upload/fileUpload/"
